c0b6bd1ac2e8133b308055e265798363c43611e0
Zsolt Toth <gerzson63@gmail.com>
2020-02-19 14:43:54 +0100

    removed outdated comments

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-rem Run this batch as administrator
+
 WinService\bin\debug\winservice.exe
 pause
--- stop.bat
+++ stop.bat
@@ -1,3 +1,2 @@
-rem Run this batch from the Visual Studio command prompt as administrator
 taskkill /IM winservice.exe /F
 pause
